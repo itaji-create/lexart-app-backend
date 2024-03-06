df96288c2360f77fbe451a37663167ace5736e48
itaji-create <itaji.1999@gmail.com>
2024-03-06 16:57:16 -0300

    coloca todas as rotas de volta

--- src/api/app.js
+++ src/api/app.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path')
-const { User } = require('../database/models')
 const app = express();
-// const routes = require('../routes/index')
+const routes = require('../routes/index')
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.get('/', (_req, res) => res.status(200).json("Your coffee Sir"));
-// routes(app);
-app.get('/users', async (_req, res) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    console.error('Erro ao buscar usuários:', error);
-    res.status(500).json({ error: 'Erro ao buscar usuários' });
-  }
-});
+routes(app);
 
 module.exports = app;
