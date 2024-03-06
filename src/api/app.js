4e7fe2bc3b2d00130ee7b9575c37ae06734b3762
itaji-create <itaji.1999@gmail.com>
2024-03-05 23:34:42 -0300

    adiciona rotas

--- src/api/app.js
+++ src/api/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 // const cors = require('cors');
 
 const app = express();
-// const routes = require('../routes/index')
+const routes = require('../routes/index')
 
 app.use(express.json());
 // app.use(cors());
 
 app.get('/', (_req, res) => res.status(200).json("Your coffee Sir"));
-// routes(app);
+routes(app);
 
 module.exports = app;
