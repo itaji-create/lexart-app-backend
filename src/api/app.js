3d1a491b651bc8b27ba73aa57b63880cf874ef5f
itaji-create <itaji.1999@gmail.com>
2024-03-06 16:32:23 -0300

    adiciona função mais basica para retornas usuarios

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path')
+const { User } = require('../database/models')
 const app = express();
 // const routes = require('../routes/index')
 
@@ -10,5 +11,14 @@ app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.get('/', (_req, res) => res.status(200).json("Your coffee Sir"));
 // routes(app);
+app.get('/users', async (_req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+});
 
 module.exports = app;
