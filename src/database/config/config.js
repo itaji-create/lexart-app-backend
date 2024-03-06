1cf6fbc0e7bec6ef737238553709349254d88a38
itaji-create <itaji.1999@gmail.com>
2024-03-06 11:58:34 -0300

    simplifica configurações de model

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,40 +1,17 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "development";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
 const options = {
   host: process.env.PGHOST || 'localhost',
   port: process.env.PGPORT || '5432',
   database: 
-    `${process.env.PGDATABASE || 'lexart-database'}${suffix[environment] || suffix.test}`,
+    `${process.env.PGDATABASE || 'database'}`,
   username: process.env.PGUSER || 'postgres',
-  password: process.env.PGPASSWORD || '89012345',
+  password: process.env.PGPASSWORD || 'password',
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
-    ssl: {
-      require: true,
-    },
   },
   logging: false,
 };
 
-module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-  production: {
-    ...options,
-  },
-};
+module.exports = options;
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -3,33 +3,20 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const { DATABASE_URL } = process.env;
+const config = require('../config/config.js');
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(DATABASE_URL || config);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
+fs.readdirSync(__dirname)
+  .filter(file => file.endsWith('.js') && file !== 'index.js')
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+Object.values(db).forEach(model => model.associate && model.associate(db));
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
