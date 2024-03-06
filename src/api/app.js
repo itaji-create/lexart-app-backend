daf0b0353d73b1ed87d3dd0479073d448ec82f48
itaji-create <itaji.1999@gmail.com>
2024-03-06 00:26:23 -0300

    adiciona mapeamento do static

--- src/api/app.js
+++ src/api/app.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const { getUsers } = require('../controllers/user.controller');
+const path = require('path')
 
 const app = express();
 // const routes = require('../routes/index')
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.get('/', (_req, res) => res.status(200).json("Your coffee Sir"));
-app.get('/users', async (_req, res) => {
-  try {
-    const users = await getUsers();
-    res.status(200).json(users);
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 // routes(app);
 
 module.exports = app;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,7 +33,7 @@ const deleteUser = async (req, res) => {
 const getUsers = async (req, res) => {
   try {
     const users = await loginService.getUsers();
-    return users;
+    return res.status(200).json(users);
   } catch (error) {
     return res.status(409).json({ message: error.message })
   }
