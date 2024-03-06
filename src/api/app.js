c811f3a650b57537962f9eaeccdb244a7d1fa41e
itaji-create <itaji.1999@gmail.com>
2024-03-06 17:18:24 -0300

    adiciona configurações de cors

--- src/api/app.js
+++ src/api/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path')
 const app = express();
-const routes = require('../routes/index')
-
+const routes = require('../routes/index');
+const corsOptions = {
+  origin: 'https://lexart-app-frontend.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.get('/', (_req, res) => res.status(200).json("Your coffee Sir"));
