29dd3a2b7a0fe19e1857ee529346c82ea504e80e
itaji-create <itaji.1999@gmail.com>
2024-03-05 22:12:17 -0300

    remove rotas do server para realizar um teste no versel

--- src/api/app.js
+++ src/api/app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path')
 
 const app = express();
-const routes = require('../routes/index')
+// const routes = require('../routes/index')
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.get('/', (_req, res) => res.status(200).json("Your coffee Sir"));
-routes(app);
+// routes(app);
 
 module.exports = app;
